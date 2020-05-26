80526c0c460df3c138b584b7b88a248018e9dec3
Mateus <mateus.metz@hbsis.com.br>
2020-05-26 14:26:32 -0300

    register controller

---
+++ src/controllers/register_controller.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const User = require("../models/user");
+
+const router = express.Router();
+
+router.post("/register", async (req, res) => {
+  try {
+    const user = await User.create(req.body);
+    return res.send({ user });
+  } catch (error) {
+    return res.status(400).send({ error: "Failed register" });
+  }
+});
