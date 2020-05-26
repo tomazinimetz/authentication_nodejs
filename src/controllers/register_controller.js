bd47532b9628d85666794b9d11f918777442edd0
Mateus <mateus.metz@hbsis.com.br>
2020-05-26 15:04:31 -0300

    feat:cant register if email already exists

--- src/controllers/register_controller.js
+++ src/controllers/register_controller.js
@@ -3,13 +3,18 @@ const User = require("../models/user");
 
 const router = express.Router();
 
-router.post('/register', async (req, res) => {
+router.post("/register", async (req, res) => {
+  const { email } = req.body;
   try {
+    if (await User.findOne({ email })) {
+      return res.status(400).send({ error: "Email already in use" });
+    }
     const user = await User.create(req.body);
+    user.password = undefined;
     return res.send({ user });
   } catch (err) {
     return res.status(400).send({ error: "Failed register" });
   }
 });
 
-module.exports = app => app.use('/auth', router);
+module.exports = (app) => app.use("/auth", router);
